91e4e14ea2415081b50effeed581cf804ef4c11a
Viruthika M <113979852+viruthi-25@users.noreply.github.com>
2023-08-31 16:28:00 +0530

    Create Programming Assignment 4.c
    Write a C Program to find the Largest Number (integer)  among Three Numbers (integers) using IF and Logical && operator.

---
+++ Week 3/Programming Assignment 4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, n3;
+    scanf("%d %d %d", &n1, &n2, &n3); 
+    if( n1>=n2 && n1>=n3 )
+        printf("%d is the largest number.", n1);
+    if( n2>=n1 && n2>=n3 )
+        printf("%d is the largest number.", n2);
+    if( n3>=n1 && n3>=n2 )
+        printf("%d is the largest number.", n3);
+}
