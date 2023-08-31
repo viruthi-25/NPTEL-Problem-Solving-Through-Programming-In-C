adf20325585c3dc5f78940c83ea1d78dd28c81bd
Viruthika M <113979852+viruthi-25@users.noreply.github.com>
2023-08-31 16:32:30 +0530

    Create Programming Assignment 1.c
    To write a C Program to Find the Smallest Number among Three Numbers (integer values) using Nested IF-Else statement.

---
+++ Week 4/Programming Assignment 1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, n3; 
+    scanf("%d %d %d", &n1, &n2, &n3); 
+    int small;
+    if(n1<n2)
+    {
+      if(n1<n3)
+      {
+        small=n1;
+      }
+      else
+      {
+        small=n3;
+      }
+    }
+    else
+    {
+      if(n2<n3)
+      {
+        small=n2;
+      }
+      else
+      {
+        small=n3;
+      }
+    }
+    printf("%d is the smallest number.", small);
+}
